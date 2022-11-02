3a12808e19579ec7b2f8548ac78baa74865cfbf9
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-11-02 20:41:30 -0300

    Added -- Random enemy selection

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,23 +1,43 @@
 function beginGame() {
     let buttonPlayerMascot = document.getElementById('boton-mascota');
-    buttonPlayerMascot.addEventListener('click', selectPlayerMascot);
+    buttonPlayerMascot.addEventListener('click', selectPlayerPet);
 }
 
-function selectPlayerMascot() {
+function selectPlayerPet() {
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
-    let spanMascotaJugador = document.getElementById('mascota-jugador');
+    let spanPlayerPet = document.getElementById('mascota-jugador');
+    beginEnemySelection = true
 
     if (inputHipodoge.checked) {
-        spanMascotaJugador.innerHTML = 'Hipodoge';
+        spanPlayerPet.innerHTML = 'Hipodoge';
     } else if (inputCapipepo.checked) {
-        spanMascotaJugador.innerHTML = 'Capipepo';
+        spanPlayerPet.innerHTML = 'Capipepo';
     } else if (inputRatigueya.checked) {
-        spanMascotaJugador.innerHTML = 'Ratigueya';
+        spanPlayerPet.innerHTML = 'Ratigueya';
     } else {
-        alert('Selecciona una mascota, por favor')
+        alert('Selecciona una mascota, por favor');
+        beginEnemySelection = false
     }
+    if (beginEnemySelection == true) {selectEnemyPet();}
+}
+
+function selectEnemyPet() {
+    let randomAttack = random(1, 3);
+    let spanEnemyPet = document.getElementById('mascota-enemiga');
+
+    if (randomAttack == 1) {
+        spanEnemyPet.innerHTML = 'Hipodoge';
+    } else if (randomAttack == 2) {
+        spanEnemyPet.innerHTML = 'Capipepo';
+    } else {
+        spanEnemyPet.innerHTML = 'Ratigueya';
+    }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener('load', beginGame);
