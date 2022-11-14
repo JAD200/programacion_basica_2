0401b5d1b08304518b9228272e4a2776ceb8c96e
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-11-14 15:25:19 -0300

    Applied -- DRY method

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,39 +1,50 @@
+// HTML elements
+const selectAttackSection = document.getElementById('seleccionar-ataque');
+
+const resetSection = document.getElementById('reiniciar');
+
+const playerMascotButton = document.getElementById('boton-mascota');
+const fireButton = document.getElementById('boton-fuego');
+const waterButton = document.getElementById('boton-agua');
+const groundButton = document.getElementById('boton-tierra');
+const resetGameButton = document.getElementById('boton-reset');
+
+const selectPetSection = document.getElementById('seleccionar-mascota');
+const inputHipodoge = document.getElementById('hipodoge');
+const inputCapipepo = document.getElementById('capipepo');
+const inputRatigueya = document.getElementById('ratigueya');
+const spanPlayerPet = document.getElementById('mascota-jugador');
+
+const spanEnemyPet = document.getElementById('mascota-enemiga');
+
+const spanPlayerLives = document.getElementById('vidas-mascota-jugador');
+const spanEnemyLives = document.getElementById('vidas-mascota-enemiga');
+
+const messagesSection = document.getElementById('resultado');
+const playerAttackMessage = document.getElementById('ataques-del-jugador');
+const enemyAttackMessage = document.getElementById('ataques-del-enemigo');
+
 let playerAttack;
 let enemyAttack;
 let playerLives = 3;
 let enemyLives = 3;
 
 function beginGame() {
-    let selectAttackSection = document.getElementById('seleccionar-ataque');
     selectAttackSection.style.display = 'none';
-
-    let resetSection = document.getElementById('reiniciar');
     resetSection.style.display = 'none';
-
-    let playerMascotButton = document.getElementById('boton-mascota');
     playerMascotButton.addEventListener('click', selectPlayerPet);
 
     // Attack buttons
-    let fireButton = document.getElementById('boton-fuego');
     fireButton.addEventListener('click', fireAttack);
-    let waterButton = document.getElementById('boton-agua');
     waterButton.addEventListener('click', waterAttack);
-    let groundButton = document.getElementById('boton-tierra');
     groundButton.addEventListener('click', groundAttack);
 
-    let resetGameButton = document.getElementById('boton-reset');
     resetGameButton.addEventListener('click', resetGame);
 }
 
 function selectPlayerPet() {
-    let selectPetSection = document.getElementById('seleccionar-mascota');
     selectPetSection.style.display = 'none';
-    let selectAttackSection = document.getElementById('seleccionar-ataque');
 
-    let inputHipodoge = document.getElementById('hipodoge');
-    let inputCapipepo = document.getElementById('capipepo');
-    let inputRatigueya = document.getElementById('ratigueya');
-    let spanPlayerPet = document.getElementById('mascota-jugador');
 
     if (inputHipodoge.checked) {
         spanPlayerPet.innerHTML = 'Hipodoge';
@@ -52,7 +63,6 @@ function selectPlayerPet() {
 
 function selectEnemyPet() {
     let randomPet = random(1, 3);
-    let spanEnemyPet = document.getElementById('mascota-enemiga');
 
     if (randomPet == 1) {
         spanEnemyPet.innerHTML = 'Hipodoge';
@@ -93,9 +103,6 @@ function selectEnemyAttack() {
 }
 
 function combat() {
-    let spanPlayerLives = document.getElementById('vidas-mascota-jugador');
-    let spanEnemyLives = document.getElementById('vidas-mascota-enemiga');
-
     if (enemyAttack == playerAttack) {
         createMessage('EMPATE');
     } else if (playerAttack == 'FUEGO' && enemyAttack == 'TIERRA') {
@@ -128,10 +135,6 @@ function checkLives() {
 }
 
 function createMessage(combatResult) {
-    let messagesSection = document.getElementById('resultado');
-    let playerAttackMessage = document.getElementById('ataques-del-jugador');
-    let enemyAttackMessage = document.getElementById('ataques-del-enemigo');
-
     let newPlayerAttack = document.createElement('p');
     let newEnemyAttack = document.createElement('p');
 
@@ -144,18 +147,12 @@ function createMessage(combatResult) {
 }
 
 function createFinalMessage(finalResult) {
-    let resetSection = document.getElementById('reiniciar');
     resetSection.style.display = 'block';
-
-    let messagesSection = document.getElementById('resultado');
 
     messagesSection.innerHTML = finalResult;
 
-    let fireButton = document.getElementById('boton-fuego');
     fireButton.disabled = true;
-    let waterButton = document.getElementById('boton-agua');
     waterButton.disabled = true;
-    let groundButton = document.getElementById('boton-tierra');
     groundButton.disabled = true;
 }
 
